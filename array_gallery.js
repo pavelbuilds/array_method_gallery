82e5d21faaea31fc03abcf614908cdb165922162
Pavel Kickler <Pascha@Pavels-MacBook-Pro.local>
2021-03-03 13:37:34 +0100

    functionality ready, array explanations missing

---
+++ array_gallery.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+   const thisPanel = this;
+   panels.forEach(panel => {
+      if(panel != thisPanel){
+         if(panel.classList.contains("open")){
+            panel.classList.remove("open");
+            panel.classList.remove("open-active");
+         }
+      }
+   })
+   this.classList.toggle("open");
+   this.classList.toggle("open-active");
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
