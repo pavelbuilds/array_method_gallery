a8d9b81a04240e75dc454d745c4a3a0e43a24340
Pavel <79279743+JuicyCodeDev@users.noreply.github.com>
2021-03-05 14:00:18 +0100

    Add files via upload

---
+++ array_gallery.js
@@ -0,0 +1,19 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+   const thisPanel = this;
+   panels.forEach(panel => {
+      if(panel != thisPanel){
+         if(panel.classList.contains("open")){
+            panel.classList.remove("open");
+            panel.classList.remove("open-active");
+         }
+      }
+   })
+   this.classList.toggle("open");
+   this.classList.toggle("open-active");
+}
+
+panels.forEach(panel => panel.addEventListener("click", toggleOpen));
+
+// That a test  I hope it works
